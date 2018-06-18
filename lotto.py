db2ab0bdba25f143d6d3cc9520ec4d3d39a5fb9d
Paul Ellis <golgothen@gmail.com>
2018-06-18 18:29:02 +1000

    First commit

---
+++ lotto.py
@@ -0,0 +1,311 @@
+import csv, operator, multiprocessing, os
+
+from datetime import datetime
+from itertools import combinations
+from vector import vector
+from sys import stdout
+
+#from numba import jit
+
+class Ball():
+    def __init__(self, v):
+        self.value = v
+        self.drawnWith = vector(45)
+        self.supWith = vector(45)
+        self.lastDrawn = datetime(1900,1,1)
+        self.streaks = vector(1)
+        self.nonStreaks = vector(1)
+        self.drawStreaks = vector(1)
+        
+    #@jit(nonpython=true)
+    def draw(self, game):
+        if self.value in game.numbers + game.sups:
+            if self.nonStreaks[0] != 0:
+                self.nonStreaks.insert(0,0)
+            self.streaks[0] += 1
+            if self.value in game.numbers:
+                self.drawStreaks[0] += 1
+            if game.drawDate > self.lastDrawn:
+                self.lastDrawn = game.drawDate
+            for a in game.numbers:
+                self.drawnWith[a-1] += 1
+            for a in game.sups:
+                #self.drawnWith[a-1] += 1
+                self.supWith[a-1] += 1
+        else:
+            if self.streaks[0] != 0:
+                self.streaks.insert(0,0)
+            if self.drawStreaks[0] != 0:
+                self.drawStreaks.insert(0,0)
+            self.nonStreaks[0] += 1
+    
+    @property
+    def count(self):
+        return self.drawnWith[self.value-1]
+    
+    @property
+    def sup(self):
+        return  self.supWith[self.value-1]
+    
+    @property
+    def strikeRate(self):
+        return (self.count + self.sup)/(sum(self.streaks)+sum(self.nonStreaks))*100
+    
+    def __str__(self):
+        s =  'Ball {}:\n'.format(self.value)
+        s += '  Drawn {} times, Supped {} times, {} in total, strike rate of {:3.3f}%, last drawn on {}\n'.format(self.count,self.sup, self.count + self.sup, self.strikeRate, self.lastDrawn)
+        s += '  Current draw streak: {}, Longest draw streak: {}, Average draw streak: {:2.3f}\n'.format(self.drawStreaks[0],max(self.drawStreaks),self.drawStreaks.mean)
+        s += '  Current pick streak: {}, Longest pick streak: {}, Average pick streak: {:2.3f}\n'.format(self.streaks[0],max(self.streaks),self.streaks.mean)
+        s += '  Current miss streak: {}, Longest miss streak: {}, Average miss streak: {:2.3f}\n'.format(self.nonStreaks[0],max(self.nonStreaks),self.nonStreaks.mean)
+        s += '  Top 10 most commonly drawn with - Ball(Count):\n    '
+        t = self.drawnWith.sort(True)
+        for i in range(1, 11):
+            s += '{}({}), '.format(t[i][0]+1, t[i][1])
+        s = s[:-2] + '\n'
+        s += '  Top 10 most commonly supplemented with - Ball(Count):\n    '
+        t = self.supWith.sort(True)
+        for i in range(1, 11):
+            s += '{}({}), '.format(t[i][0]+1, t[i][1])
+        s = s[:-2] + '\n'
+        return s
+
+class Field():
+    def __init__(self):
+        self.balls = {}
+        for a in range(1,46):
+            self.balls[a] = Ball(a)
+    
+    def draw(self, game):
+        for b in self.balls.values():
+            b.draw(game)
+
+class Game():
+    def __init__(self, data):
+        self.day = data[0]
+        self.drawDate = datetime.strptime(data[1],"%d/%m/%Y")
+        self.numbers = [int(i) for i in data[2:8]]
+        self.sups = [int(i) for i in data[8:10]]
+    
+    #@jit(nonpython=true)
+    def play(self, numbers):
+        drawcount = 0
+        supcount = 0
+        if len(self.numbers) == 6 and len(self.sups) == 2 and len(numbers) > 5:
+            for n in numbers:
+                if n in self.numbers:
+                    drawcount += 1
+                if n in self.sups:
+                    supcount += 1
+                    #print('Draw count: {}, Sup Count: {}'.format(drawcount,supcount))
+            if drawcount == 6:
+                return 1
+            if drawcount == 5:
+                if supcount > 0:
+                    return 2
+                return 3
+            if drawcount == 4:
+                return 4
+            if drawcount == 3 and supcount > 0:
+                return 5
+            if drawcount > 0 and supcount == 2:
+                return 6
+            return None
+
+class Cruncher(multiprocessing.Process):
+    def __init__(self, workerCount, poolSize, blockSize, games, jobQ, resultQ, divisionWeights = None):
+        super(Cruncher,self).__init__()
+        self.poolSize = poolSize
+        self.blockSize = blockSize
+        self.games = games
+        self.jobQ = jobQ
+        self.resultQ = resultQ
+        self.bestWeight = 0
+        self.workerCount = workerCount
+        if divisionWeights is None:
+            self.weights = vector([95000, 750, 100, 3, 2, 1])
+        else:
+            self.weights = vector(divisionWeights)
+    
+    #@jit(nonpython=true)
+    def run(self):
+        print('Cruncher {} starting with divisional weights {}'.format(self.workerCount, self.weights))
+        while True:
+            c = 0
+            t = datetime.now()
+            prefix = self.jobQ.get()
+            if prefix is None:
+                return None
+            #self.resultQ.put(Message('M', self.workerCount, 'Starting block {}'.format(prefix)))
+            for i in combinations(range(max(list(prefix))+1,self.poolSize+1),self.blockSize):
+                r = {}
+                r['Numbers'] = prefix + i
+                r['Divisions'] = vector(6)
+                r['Process'] = self.workerCount
+                for g in self.games:
+                    d = g.play(r['Numbers'])
+                    c += 1
+                    if d is not None:
+                         r['Divisions'][d-1] += 1
+                r['Weight'] = sum(r['Divisions'] * self.weights)
+                if r['Weight'] > self.bestWeight:
+                    self.bestWeight = r['Weight']
+                    self.resultQ.put(Message('R', self.workerCount,r))
+            e = datetime.now() - t
+            s = e.seconds + (e.microseconds / 1000000)
+            if s  > 0:
+                self.resultQ.put(Message('M',self.workerCount, 'Completed {:10,.0f} combinations in {:8,.4f} seconds. {:7,.0f} combinations per second. Block {}'.format(c, s, c/s, prefix)))
+                
+class Message():
+    def __init__(self, type, id, message):
+        self.type = type
+        self.id = id
+        self.message = message
+
+class Procs():
+    def __init__(self, id):
+        self.id = id
+        self.lastMessage = ''
+        self.lastResult = {'Numbers' : vector(7), 'Divisions' : vector(6), 'Weight' : 0}
+        self.bestResult = {'Numbers' : vector(7), 'Divisions' : vector(6), 'Weight' : 0}
+        
+class Results():
+    def __init__(self, games):
+        self.games = games
+        self.field = 45
+        self.block = 4
+        self.pick = 7
+        self.divisionWeights = vector([95000, 750, 100, 3, 2, 1])
+    
+    def compute(self, picks = None, block = None, weights = None):
+        if picks is not None:
+            self.pick = picks
+        if block is not None:
+            self.block = block
+        if weights is not None:
+            self.divistionWeights = vector(weights)
+        print('Division weights {}'.format(self.divisionWeights))
+        print('Division argument {}'.format(weights))
+        workQ = multiprocessing.Queue()
+        resultQ = multiprocessing.Queue()
+        
+
+        for i in combinations(range(1,self.field - self.block + 1),self.pick - self.block):
+            workQ.put(i)
+        for i in range(multiprocessing.cpu_count()):
+            workQ.put(None)
+        print('Added {:15,.0f} work blocks to the work que'.format(workQ.qsize()))
+
+        crunchers = []
+        procs = []
+        for i in range(multiprocessing.cpu_count()):
+            crunchers.append(Cruncher(i, self.field, self.block, self.games, workQ, resultQ, self.divisionWeights))
+            crunchers[i].daemon = True
+            crunchers[i].start()
+            procs.append(Procs(i))
+            
+        print("{} crunchers started.".format(multiprocessing.cpu_count()))
+        
+        while not workQ.empty():
+            m = resultQ.get()
+            if m.type == 'M':
+                procs[m.id].lastMessage = m.message
+                print('{}. {:9,.0f} blocks left'.format(m.message, workQ.qsize()))
+            if m.type == 'R':
+                procs[m.id].lastResult = m.message
+                if procs[m.id].lastResult['Weight'] > procs[m.id].bestResult['Weight']:
+                    procs[m.id].bestResult = procs[m.id].lastResult.copy()
+                    #print('Numbers = {}, Divisions = {}, Weight = {}'.format(m.message['Numbers'], m.message['Divisions'], m.message['Weight']))
+                    with open('Results.txt','a') as f:
+                        f.write('Numbers = {}, Divisions = {}, Weight = {}.\n'.format(m.message['Numbers'], m.message['Divisions'], m.message['Weight']))
+            #updateScreen(procs,workQ.qsize())
+                
+        for i in range(multiprocessing.cpu_count()):
+            crunchers[i].join()
+
+def prep():
+    games = []
+    
+    with open("lotto.csv") as f:
+        reader = csv.reader(f)
+        headings = next(reader)
+        
+        for row in reader:
+            if row[0] in ['SAT']:  # + ['WED','MON']
+                games.append(Game(row))
+    return games
+
+def paintScreen(procs):
+    os.system('cls')
+    stdout.write('-- ID --:' + ('-' * 90) + '\n')
+    for p in procs:
+        stdout.write('  {:2.0f}  : Best - '.format(p.id))
+        for n in p.bestResult['Numbers']:
+            stdout.write('{:2.0f}, '.format(p.bestResult['Numbers'][n]))
+        stdout.write(' : Divisions - ')
+        for n in p.bestResult['Divisions']:
+            stdout.write('{:3.0f}, '.format(p.bestResult['Divisions'][n]))
+        stdout.write('/n')
+        stdout.write('      : Last - '.format(p.id))
+        for n in p.lastResult['Numbers']:
+            stdout.write('{:2.0f}, '.format(p.lastResult['Numbers'][n]))
+        stdout.write(' : Divisions - ')
+        for n in p.lastResult['Divisions']:
+            stdout.write('{:3.0f}, '.format(p.lastResult['Divisions'][n]))
+        stdout.write('/n')
+        stdout.write('      : Message - {}/n'.format(p.lastMessage))
+        stdout.write('--------:' + ('-' * 90) + '\n')
+
+
+def updateScreen(procs,qsize):
+    
+    for p in procs:
+        
+        s = ''
+        for j in p.bestResult['Numbers']:
+            s += '{:2.0f}, '.format(j)
+        printxy(1+(p.id*4),16,s)
+
+        s = ''
+        for j in p.bestResult['Divisions']:
+            s += '{:3.0f}, '.format(j)
+        printxy(2+(p.id*4),31 + (len(p.bestResult['Numbers'])*4),s)
+
+def printxy(x, y, text):
+    stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
+    stdout.flush()
+
+def go():
+    g = prep()
+    r = Results(g)
+    r.compute(10,4,vector([0,750,100,3,2,1]))
+
+if __name__ == '__main__':
+    go()
+    
+
+#    field = Field()
+#    for a in range(len(games)-1, -1, -1):   # Itterate the list backward
+#        field.draw(games[a])
+#    
+#    m = []
+#    for b, v in field.balls.items():
+#        m.append(v.drawnWith)
+#    s = '  : '
+#    for a in range(45):
+#        s += '{:2.0f} '.format(a+1)
+#    print(s)
+#    for a in range(45):
+#
+#     s = '{:2.0f}  '.format(a+1)
+#        for b in range(45):
+#            if a==b:
+#                s += '   '
+#            else:
+#                s += '{:2.0f} '.format(m[a][b])
+#        print(s)
+#    
+#    for g in games:
+#        d = g.play((1, 15, 11, 42, 31, 12, 36))
+#        if d is not None:
+#            print('Game {} paid dividend {}'.format(g.drawDate, d))
+    
---
+++ vector.py
@@ -0,0 +1,90 @@
+class vector(list):
+    def __init__(self, initSize = None):
+        super(list, self).__init__()
+        if initSize is not None:
+            if type(initSize).__name__ == 'int':
+                for a in range(initSize):
+                    self.append(0)
+            if type(initSize).__name__ in ['list', 'tuple', 'vector']:
+                for a in initSize:
+                    self.append(a)
+    
+    @property
+    def mean(self):
+        if len(self)>0:
+            return sum(self)/len(self)
+        else:
+            return 0
+    
+    def sort(self, desc = True):
+        s = vector()
+        for i in range(self.size):
+            s.append((i,self[i]))
+        s = sorted(s, key = operator.itemgetter(1), reverse = desc)
+        return s
+    
+    @property
+    def n(self):
+        return len(self)
+    
+    def __add__(self, v):
+        #
+        # Addition of vectors
+        #
+        r = vector()
+        if type(v).__name__ == 'vector':        # If adding two vectors
+            for i in range(self.n):             # Iterate through the vector elements
+                r.append(self[i] + v[i])        # Add the two elements together
+        else:                                   # If adding a scalar
+            for i in range(self.n):             # Iterate through the vector elements
+                r.append(self[i] + v)           # Add the scalar to each element
+        return r                                # Return the new vector
+    
+    def __mul__(self, v):
+        #
+        # Multiplication of vectors
+        #
+        r = vector()
+        if type(v).__name__ == 'vector':        # If multiplying two vectors
+            for i in range(self.n):             # Iterate through the vector elements
+                r.append(self[i] * v[i])        # Multiply the two elements together
+        else:                                   # If multiplying a scalar
+            for i in range(self.n):             # Iterate through the vector elements
+                r.append(self[i] * v)           # Multiply each element by the scalar
+        return r                                # Return the new vector
+
+    def __sub__(self, v):
+        #
+        # Subtraction of vectors
+        #
+        r = vector()
+        if type(v).__name__ == 'vector':        # If subtracting two vectors
+            for i in range(self.n):             # Iterate through the vector elements
+                r.append(self[i] - v[i])        # Subtract the two elements
+        else:                                   # If subtracting a scalar
+            for i in range(self.n):             # Iterate through the vector elements
+                r.append(self[i] - v)           # Subtract the scalar from each element
+        return r                                # Return the new vector
+
+    def __truediv__(self, v):
+        #
+        # Addition of vectors
+        #
+        r = vector()
+        if type(v).__name__ == 'vector':        # If dividing two vectors
+            for i in range(self.n):             # Iterate through the vector elements
+                r.append(self[i] / v[i])        # Divide the two elements
+        else:                                   # If dividing by a scalar
+            for i in range(self.n):             # Iterate through the vector elements
+                r.append(self[i] / v)           # Divide each element by the scalar
+        return r                                # Return the new vector
+    
+    def __pow__(self, v):
+        r = vector()
+        for i in range(self.n):
+            r.append(self[i]**v)
+        return r
+
+    @property
+    def mean(self):
+        return(sum(self)/self.n)
